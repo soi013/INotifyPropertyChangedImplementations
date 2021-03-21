24864debe00208914d44e25bbf04d91ede651a57
soi013 <soi2flatline@gmail.com>
2021-03-21 21:12:39 +0900

    GalaSoft.MvvmLightからMicrosoft.Toolkit.Mvvmへ変更

--- Source/INotifyPropertyChangedImplementations/MainWindowViewModel.cs
+++ Source/INotifyPropertyChangedImplementations/MainWindowViewModel.cs
@@ -20,13 +20,13 @@ namespace INotifyPropertyChangedImplementations
         //= new Person3();  //C#3版
         //= new Person5();    //C#5版
         //= new Person6();  //C#6版
-        = new Person7();  //C#7版
+        //= new Person7();  //C#7版
         //= new PersonVM(); //独自ViewModel継承版
         //= new PersonNB(); //独自ViewModel継承バッキングフィールド無し版
         //= new PersonVM(); //独自ViewModel継承版
         //= new PersonEx(); //拡張メソッド使用版
         //= new PersonMV(); //MVVMライブラリ使用版
-        //= new PersonRP(); //ReactiveProperty版
+        = new PersonRP(); //ReactiveProperty版
         //= new FodyPerson.PersonFD(); //Fody使用版
     }
     #region 通知なし
@@ -368,7 +368,7 @@ public class PersonX : INotifyPropertyChanged
     #endregion
 
     #region MVVMライブラリで継承版
-    public class PersonMV : GalaSoft.MvvmLight.ViewModelBase
+    public class PersonMV : Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject
     {
         private string _Name = "Hejlsberg";
         public string Name
@@ -376,8 +376,8 @@ public class PersonX : INotifyPropertyChanged
             get => _Name;
             set
             {
-                if (Set(ref _Name, value))
-                    RaisePropertyChanged(nameof(FullName));
+                if (SetProperty(ref _Name, value))
+                    OnPropertyChanged(nameof(FullName));
             }
         }
 
